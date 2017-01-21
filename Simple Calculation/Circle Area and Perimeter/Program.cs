8c7d8f2129755332d539727d2dcd9b144550da5d
Rmitrev <g_m_r@abv.bg>
2017-01-21 09:57:20 +0200

    Adding new programs in Programming Basics

---
+++ Simple Calculation/Circle Area and Perimeter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Area_and_Perimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double circle = double.Parse(Console.ReadLine());
+            double circleArea = (Math.PI * circle) * circle;
+            double circlePerimeter = (2 * Math.PI) * circle;
+            Console.WriteLine("Area = {0}", circleArea);
+            Console.WriteLine("Perimeter = {0}", circlePerimeter);
+        }
+    }
+}
