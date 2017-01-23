d34661814c5263e0da1904866e93e69dd3732c4c
Rmitrev <g_m_r@abv.bg>
2017-01-23 21:23:38 +0200

    Add new Programs

---
+++ Simple Calculation/Celsius to Fahrenheit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celsius_to_Fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float celsius = float.Parse(Console.ReadLine());
+            double farentheit = (celsius * 1.8) + 32;
+            Console.WriteLine(farentheit);
+        }
+    }
+}
---
+++ Simple Calculation/Triangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float triangleWheight = float.Parse(Console.ReadLine());
+            float triangleHeight = float.Parse(Console.ReadLine());
+            float triangleArea = (triangleWheight * triangleHeight) / 2;
+            Console.WriteLine("{0:f2}",triangleArea);
+        }
+    }
+}
